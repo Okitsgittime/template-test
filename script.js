c776fe0501d33c9b5c78445313989e49f84cc0d6
Jen <message2jens@gmail.com>
2022-05-29 08:20:46 -0400

    quote-generator

---
+++ script.js
@@ -0,0 +1,71 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// let apiQuotes = [];
+
+
+function showLoadingSpiner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+
+function removeLoadingSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes or localQuotes array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //  Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  } 
+  // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+
+
+// Get Quotes From API
+// async function getQuotes() {
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//       const response = await fetch(apiUrl);
+//       apiQuotes = await response.json();
+//       newQuote();
+//     } catch (error) {
+//        // Catch Error Here
+//     }
+// }
+
+//  Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//  Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+//getQuotes();
+showLoadingSpiner();
+setTimeout(newQuote, 1000);
+//  newQuote();
